870d54cf04e3bd2ab243afa9e2da503d0d451212
祖程 <atinzulin@hotmail.com>
2026-04-14 17:16:06 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcThreeLayerDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
